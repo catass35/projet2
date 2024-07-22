9a248d5c51401a0b09d9f983fef5a92dd86643bc
catass35 <175985261+catass35@users.noreply.github.com>
2024-07-22 21:33:37 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -20,31 +20,36 @@ worker_ips="worker ip $(terraform output worker_ips | tr -d '"')"
 # Use a here document to write multiple lines to the file, including variables
 cat <<EOF > "$output_file"
 ******************************************************
-** $jumphost_ips **
-**                                                  **
-** $nat_gateway_ips **
-**                                                  **
-** $ansible_ips **
-**                                                  **
-** $master_ips **
-**                                                  **
-** $worker_ips **
+
+$jumphost_ips
+
+$nat_gateway_ips
+
+$ansible_ips
+
+$master_ips
+
+$worker_ips
+
 ******************************************************
 EOF
 
 # Copy file to jumphost
 # Define variables
 remote_user="ubuntu"
-remote_host=$(terraform output jumphost_ip | tr -d '"')"
-remote_file="/etc/ssh/ssh_banner"
+remote_host=$(terraform output jumphost_ip | tr -d '"')
+remote_file="/home/ubuntu"
 local_destination="ssh_banner"
 
 # Use scp with StrictHostKeyChecking=no to copy the file
-scp -o StrictHostKeyChecking=no -i key.txt "${remote_user}@${remote_host}:${remote_file}" "${local_destination}"
+scp -o StrictHostKeyChecking=no -i key.txt "${local_destination}" "${remote_user}@${remote_host}:${remote_file}"
 
-# Update ssh banner
-ssh -o StrictHostKeyChecking=no -i key.txt "${remote_user}@${remote_host} "old_banner_line=$(/etc/ssh/sshd_config | grep Banner) | sudo sed -i "s|$old_banner_line|Banner $remote_file|g" "/etc/ssh/sshd_config" | sudo systemctl restart sshd"
+# Update ssh banner - NOT WORKING
+# ssh -o StrictHostKeyChecking=no -i key.txt ${remote_user}@${remote_host} "sudo echo 'Banner $remote_file$local_destination' >> /etc/ssh/sshd_config ; sudo systemctl restart sshd"
 
 # Echo command to connect to jumphost
 echo "To connect to jumphost 'ssh -i key.txt ubuntu@$(terraform output jumphost_ip | tr -d '"')'"
 
+# Modify ssh banner
+echo "Then edit /etc/ssh/sshd_config '#Banner /etc/issue.net' by '/home/ubuntu/ssh_banner'"
+echo 'sudo systemctl restart sshd'
